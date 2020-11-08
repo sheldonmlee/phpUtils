2e0925165aa40ae5f96af556cd84cc7df84d3a3d
Sheldon Lee <unn_w19014537@flintstone>
2020-11-08 16:35:31 +0000

    Added session.php.

---
+++ session.php
@@ -0,0 +1,20 @@
+<?php
+function initSession()
+{
+	if (session_status() != PHP_SESSION_NONE) return;
+
+	isset($_SERVER["DOCUMENT_ROOT"])? $home = $_SERVER["DOCUMENT_ROOT"] : $home = null; 
+	if (!$home) exit();
+	$session_dir = $home."/../sessionData";
+
+	ini_set("session.save_path", $session_dir);
+	session_start();
+}
+
+function readSession() {
+	initSession();
+	echo "<p>";
+	print_r($_SESSION);
+	echo "</p>";
+}
+?>
--- utils.php
+++ utils.php
@@ -1,4 +1,5 @@
 <?php
 include "db.php";
 include "html.php";
+include "session.php";
 ?>
